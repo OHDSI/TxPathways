fd4f396181c830caa24d04f3516d02ba203d0684
Lavallee,Dr.,Martin (MED MA) BIP-US-R <martin.lavallee@boehringer-ingelheim.com>
2024-08-13 12:40:16 -0400

    use bitwise in sql

--- inst/sql/treatment_history.sql
+++ inst/sql/treatment_history.sql
@@ -157,7 +157,7 @@ events AS (
 	LEFT JOIN #event_cohort_eras e ON e.subject_id = tp.subject_id
 	WHERE (e.cohort_start_date <= tp.cohort_date AND e.cohort_end_date >= tp.next_cohort_date)
 )
-SELECT cast(SUM(e.event_id) as bigint) as combo_id,  subject_id , cohort_start_date, cohort_end_date
+SELECT cast(SUM(POWER(cast(2 as bigint), e.event_id)) as bigint) as combo_id,  subject_id , cohort_start_date, cohort_end_date
 into #combo_events
 FROM events e
 GROUP BY subject_id, cohort_start_date, cohort_end_date;
